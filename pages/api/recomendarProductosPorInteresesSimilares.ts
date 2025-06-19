5d1545f7f881baa51b936be5d7481f03a7d55b33
JebGy <martinmunayco123@hotmail.com>
2025-06-19 15:32:29 -0500

    feat: Funciones de interez por categoria y de por interez similar

---
+++ pages/api/recomendarProductosPorInteresesSimilares.ts
@@ -0,0 +1,32 @@
+import { recomendarProductosPorInteresesSimilares } from "@/utils/consultarnodos";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  if (req.method === "GET") {
+    const { idUsuario } = req.query;
+    if (!idUsuario || typeof idUsuario !== "string") {
+      res.status(400).json({ error: "Falta el parámetro idUsuario" });
+      return;
+    }
+    try {
+      const productos = await recomendarProductosPorInteresesSimilares(idUsuario);
+      res.status(200).json(productos);
+    } catch (error) {
+      console.error("Error al recomendar productos por intereses similares:", error);
+      res.status(500).json({ error: "Error al recomendar productos por intereses similares" });
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+} 
